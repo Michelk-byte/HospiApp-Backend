854bafb48b5a69f9ecdc4a97655d6b703df9f852
jeanyves7 <jean_yves2017@outlook.com>
2021-03-06 22:20:40 +0200

    navigationBar

--- App.js
+++ App.js
@@ -1,15 +1,130 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import Login from "./components/Login/LoginPage";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TextInput,
-  TouchableOpacity,
-} from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View  } from 'react-native';
+import 'react-native-gesture-handler';
 
-export default function App() {
-  return <Login />;
+import * as React from 'react';
+
+import
+ MaterialCommunityIcons
+from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import {
+  NavigationContainer
+} from '@react-navigation/native';
+import {
+  createStackNavigator
+} from '@react-navigation/stack';
+import {
+  createBottomTabNavigator
+} from '@react-navigation/bottom-tabs';
+
+import HomeScreen from './components/pages/HomeScreen';
+import DetailsScreen from './components/pages/DetailsScreen';
+import ProfileScreen from './components/pages/ProfileScreen';
+import SettingsScreen from './components/pages/SettingsScreen';
+
+
+import Login from "./components/Login/LoginPage";
+
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function HomeStack() {
+  return (
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: { backgroundColor: '#42f44b' },
+          headerTintColor: '#fff',
+          headerTitleStyle: { fontWeight: 'bold' }
+        }}>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Home Page' }}/>
+        <Stack.Screen
+          name="Details"
+          component={DetailsScreen}
+          options={{ title: 'Details Page' }} />
+      </Stack.Navigator>
+  );
 }
+
+function SettingsStack() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Settings"
+      screenOptions={{
+        headerStyle: { backgroundColor: '#42f44b' },
+        headerTintColor: '#fff',
+        headerTitleStyle: { fontWeight: 'bold' }
+      }}>
+      <Stack.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{ title: 'Setting Page' }}/>
+      <Stack.Screen
+        name="Details"
+        component={DetailsScreen}
+        options={{ title: 'Details Page' }}/>
+      <Stack.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{ title: 'Profile Page' }}/>
+    </Stack.Navigator>
+  );
+}
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Feed"
+        tabBarOptions={{
+          activeTintColor: '#42f44b',
+        }}>
+        <Tab.Screen
+          name="HomeStack"
+          component={HomeStack}
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name="home"
+                color={color}
+                size={size}
+              />
+            ),
+          }}  />
+        <Tab.Screen
+          name="SettingsStack"
+          component={SettingsStack}
+          options={{
+            tabBarLabel: 'Settings',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name="settings"
+                color={color}
+                size={size}
+              />
+            ),
+          }} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  bottom: {
+   position:"absolute",
+   bottom:0
+    
+  }
+})
+
+export default App;
---
+++ components/pages/SettingsScreen.js
@@ -0,0 +1,79 @@
+
+import * as React from 'react';
+import {
+  TouchableOpacity,
+  StyleSheet,
+  View,
+  Text,
+  SafeAreaView
+} from 'react-native';
+
+const SettingsScreen = ({ route, navigation }) => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ flex: 1, padding: 16 }}>
+        <View
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <Text
+            style={{
+              fontSize: 25,
+              textAlign: 'center',
+              marginBottom: 16
+            }}>
+            You are on Setting Screen
+          </Text>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={
+              () => navigation.navigate('Home')
+            }>
+            <Text>Go to Home Tab</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={
+              () => navigation.navigate('Details')
+            }>
+            <Text>Open Detail Screen</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={
+              () => navigation.navigate('Profile')
+            }>
+            <Text>Open Profile Screen</Text>
+          </TouchableOpacity>
+        </View>
+        <Text
+          style={{
+            fontSize: 18,
+            textAlign: 'center',
+            color: 'grey'
+          }}>
+          React Native Bottom Navigation
+        </Text>
+        <Text
+          style={{
+            fontSize: 16,
+            textAlign: 'center',
+            color: 'grey'
+          }}>
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+    width: 300,
+    marginTop: 16,
+  },
+});
+export default SettingsScreen;
