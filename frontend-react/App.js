0a8ff9dce500294a5d8bef591d26d59ceb514e2e
MichelK-Byte <michel.khoury.w@hotmail.com>
2021-03-27 15:27:54 +0200

    signup form

--- frontend-react/App.js
+++ frontend-react/App.js
@@ -1,53 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View  } from 'react-native';
-import 'react-native-gesture-handler';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import "react-native-gesture-handler";
 
-import * as React from 'react';
+import * as React from "react";
 
-import
- MaterialCommunityIcons
-from 'react-native-vector-icons/MaterialCommunityIcons';
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
-import {
-  NavigationContainer
-} from '@react-navigation/native';
-import {
-  createStackNavigator
-} from '@react-navigation/stack';
-import {
-  createBottomTabNavigator
-} from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import HomeScreen from './components/pages/HomeScreen';
-import DetailsScreen from './components/pages/DetailsScreen';
-import ProfileScreen from './components/pages/ProfileScreen';
-import SettingsScreen from './components/pages/SettingsScreen';
-
+import HomeScreen from "./components/pages/HomeScreen";
+import DetailsScreen from "./components/pages/DetailsScreen";
+import ProfileScreen from "./components/pages/ProfileScreen";
+import SettingsScreen from "./components/pages/SettingsScreen";
 
 import Login from "./components/Login/LoginPage";
-
+import Signup from "./components/Login/SignupPage";
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 function HomeStack() {
   return (
-      <Stack.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          headerStyle: { backgroundColor: '#42f44b' },
-          headerTintColor: '#fff',
-          headerTitleStyle: { fontWeight: 'bold' }
-        }}>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: 'Home Page' }}/>
-        <Stack.Screen
-          name="Details"
-          component={DetailsScreen}
-          options={{ title: 'Details Page' }} />
-      </Stack.Navigator>
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerStyle: { backgroundColor: "#42f44b" },
+        headerTintColor: "#fff",
+        headerTitleStyle: { fontWeight: "bold" },
+      }}
+    >
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ title: "Home Page" }}
+      />
+      <Stack.Screen
+        name="Details"
+        component={DetailsScreen}
+        options={{ title: "Details Page" }}
+      />
+    </Stack.Navigator>
   );
 }
 
@@ -56,75 +50,82 @@ function SettingsStack() {
     <Stack.Navigator
       initialRouteName="Settings"
       screenOptions={{
-        headerStyle: { backgroundColor: '#42f44b' },
-        headerTintColor: '#fff',
-        headerTitleStyle: { fontWeight: 'bold' }
-      }}>
+        headerStyle: { backgroundColor: "#42f44b" },
+        headerTintColor: "#fff",
+        headerTitleStyle: { fontWeight: "bold" },
+      }}
+    >
       <Stack.Screen
         name="Settings"
         component={SettingsScreen}
-        options={{ title: 'Setting Page' }}/>
+        options={{ title: "Setting Page" }}
+      />
       <Stack.Screen
         name="Details"
         component={DetailsScreen}
-        options={{ title: 'Details Page' }}/>
+        options={{ title: "Details Page" }}
+      />
       <Stack.Screen
         name="Profile"
         component={ProfileScreen}
-        options={{ title: 'Profile Page' }}/>
+        options={{ title: "Profile Page" }}
+      />
     </Stack.Navigator>
   );
 }
 
+// function App() {
+//   return (
+//     <NavigationContainer>
+//       <Tab.Navigator
+//         initialRouteName="Feed"
+//         tabBarOptions={{
+//           activeTintColor: '#42f44b',
+//         }}>
+//         <Tab.Screen
+//           name="HomeStack"
+//           component={HomeStack}
+//           options={{
+//             tabBarLabel: 'Home',
+//             tabBarIcon: ({ color, size }) => (
+//               <MaterialCommunityIcons
+//                 name="home"
+//                 color={color}
+//                 size={size}
+//               />
+//             ),
+//           }}  />
+//         <Tab.Screen
+//           name="SettingsStack"
+//           component={SettingsStack}
+//           options={{
+//             tabBarLabel: 'Settings',
+//             tabBarIcon: ({ color, size }) => (
+//               <MaterialCommunityIcons
+//                 name="settings"
+//                 color={color}
+//                 size={size}
+//               />
+//             ),
+//           }} />
+//       </Tab.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
 function App() {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="Feed"
-        tabBarOptions={{
-          activeTintColor: '#42f44b',
-        }}>
-        <Tab.Screen
-          name="HomeStack"
-          component={HomeStack}
-          options={{
-            tabBarLabel: 'Home',
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons
-                name="home"
-                color={color}
-                size={size}
-              />
-            ),
-          }}  />
-        <Tab.Screen
-          name="SettingsStack"
-          component={SettingsStack}
-          options={{
-            tabBarLabel: 'Settings',
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons
-                name="settings"
-                color={color}
-                size={size}
-              />
-            ),
-          }} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
+  return <Signup />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center'
+    alignItems: "center",
   },
   bottom: {
-   position:"absolute",
-   bottom:0
-    
-  }
-})
+    position: "absolute",
+    bottom: 0,
+  },
+});
 
 export default App;
---
+++ frontend-react/components/Login/SignupPage.js
@@ -0,0 +1,112 @@
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+
+export default function Signup() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.container1}>
+        <TextInput
+          style={styles.inputBox}
+          underlineColorAndroid="rgba(0,0,0,0)"
+          placeholder="Username"
+          placeholderTextColor="#ffffff"
+          selectionColor="#fff"
+          keyboardType="email-address"
+        />
+        <TextInput
+          style={styles.inputBox}
+          underlineColorAndroid="rgba(0,0,0,0)"
+          placeholder="Email"
+          placeholderTextColor="#ffffff"
+          selectionColor="#fff"
+          keyboardType="email-address"
+        />
+        <TextInput
+          style={styles.inputBox}
+          underlineColorAndroid="rgba(0,0,0,0)"
+          placeholder="Password"
+          secureTextEntry={true}
+          placeholderTextColor="#ffffff"
+        />
+        <TextInput
+          style={styles.inputBox}
+          underlineColorAndroid="rgba(0,0,0,0)"
+          placeholder="Verify Password"
+          secureTextEntry={true}
+          placeholderTextColor="#ffffff"
+        />
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Register</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.signupTextCont}>
+        <Text style={styles.signupText}>Already have an account?</Text>
+        <TouchableOpacity>
+          <Text style={styles.signupButton}> Sign in</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container1: {
+    flexGrow: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  inputBox: {
+    width: 300,
+    height: 30,
+    backgroundColor: "rgba(255, 255,255,0.2)",
+    borderRadius: 25,
+    paddingHorizontal: 16,
+    fontSize: 16,
+    color: "#ffffff",
+    marginVertical: 13,
+  },
+  button: {
+    width: 300,
+    backgroundColor: "#1c313a",
+    borderRadius: 25,
+    marginVertical: 10,
+    paddingVertical: 13,
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: "500",
+    color: "#ffffff",
+    textAlign: "center",
+  },
+  container: {
+    backgroundColor: "#455a64",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  signupTextCont: {
+    flexGrow: 1,
+    alignItems: "flex-end",
+    justifyContent: "center",
+    paddingVertical: 16,
+    flexDirection: "row",
+  },
+  signupText: {
+    color: "rgba(255,255,255,0.6)",
+    fontSize: 16,
+  },
+  signupButton: {
+    color: "#ffffff",
+    fontSize: 16,
+    fontWeight: "500",
+  },
+});
