81274b25cff2908f8fa2e504783bbe2af5ee6497
MichelK-Byte <michel.khoury.w@hotmail.com>
2021-03-29 20:08:53 +0300

    cors

--- backend-python/Users.py
+++ backend-python/Users.py
@@ -39,11 +39,12 @@ class User:
         return redirect('/')
 
     def login(self):
+        data = request.get_json(force=True)
         user = mongo.db.users.find_one({
-            "email": request.form.get('email')
+            "email": data['email']
         })
 
-        if user and pbkdf2_sha256.verify(request.form.get('password'), user['password']):
+        if user and pbkdf2_sha256.verify(data['password'], user['password']):
             return self.start_session(user)
 
         return jsonify({"error": "Invalid login credentials"}), 401
