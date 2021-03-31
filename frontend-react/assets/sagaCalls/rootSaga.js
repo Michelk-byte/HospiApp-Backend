32be3d77540759d6980af194538703affd4a3af7
MichelK-Byte <michel.khoury.w@hotmail.com>
2021-03-31 20:22:25 +0300

    login

--- frontend-react/assets/sagaCalls/rootSaga.js
+++
@@ -1,7 +0,0 @@
-import { call, all } from "redux-saga/effects";
-import { checkInWatcher } from "./LoggInSaga";
-import { signUpWatcher } from "./SignUpReducer";
-
-export function* rootSaga() {
-  yield all([call(checkInWatcher), call(signUpWatcher)]);
-}
