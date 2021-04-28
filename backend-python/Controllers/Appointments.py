895792e02aeda8a910f732ffcd0466d84992417f
MichelK-Byte <michel.khoury.w@hotmail.com>
2021-04-28 11:06:49 +0300

    Add Appointments

--- backend-python/Controllers/Appointments.py
+++ backend-python/Controllers/Appointments.py
@@ -11,3 +11,25 @@ class Appointment:
 
         if mongo.db.appointment_hospital.insert(data, w="majority"):
             return jsonify({"message": "Appointment Booked!", "status": 200}), 200
+
+    def post_appointment_lab(self):
+        data = request.get_json(force=True)
+
+        # check doctor availibility
+
+        # check if another booked an appoitnment at the same time
+
+        if mongo.db.appointment_lab.insert(data, w="majority"):
+            return jsonify({"message": "Appointment Booked!", "status": 200}), 200
+
+    def getAll_Appointments(self, id):
+        hos_app = mongo.db.appointment_hospital.find({"userid": id})
+        lab_app = mongo.db.appointment_lab.find({"userid": id})
+
+        appointments_ = list(hos_app) + list(lab_app)
+
+        appointments = {
+            "appointments": appointments_,
+            "status": 200
+        }
+        return jsonify(appointments), 200
--- backend-python/routes/Appointments.py
+++ backend-python/routes/Appointments.py
@@ -5,5 +5,17 @@ from utils.Decorator_Login import login_required
 
 @app.route('/appointment/hospital', methods=['POST'])
 @login_required
-def appointment():
+def hos_appointment():
     return Appointment().post_appointment_hospital()
+
+
+@app.route('/appointment/lab', methods=['POST'])
+@login_required
+def lab_appointment():
+    return Appointment().post_appointment_lab()
+
+
+@app.route('/appointment/<id>', methods=['GET'])
+@login_required
+def appointment(id):
+    return Appointment().getAll_Appointments(id)
