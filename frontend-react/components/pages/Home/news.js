19a25fcff76a51c7ce910852a6c868cbe03e48df
MichelK-Byte <michel.khoury.w@hotmail.com>
2021-04-29 11:46:40 +0300

    heroku start

--- frontend-react/components/pages/Home/news.js
+++
@@ -1,7 +0,0 @@
-const url =
-    "https://newsapi.org/v2/top-headlines?country=us&category=health&apiKey=d959c31b5493474b98aab6e4497d358e";
-
-export async function getNews() {
-    let result = await fetch(url).then(response => response.json());
-    return result.articles
-}
