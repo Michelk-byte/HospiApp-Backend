54728da6beb09e8ff272147f4f753db533e855d4
MichelK-Byte <michel.khoury.w@hotmail.com>
2021-04-29 12:05:27 +0300

    heroku start

--- routes/Hospital.py
+++ routes/Hospital.py
@@ -5,20 +5,20 @@ from utils.Decorator_Login import login_required
 
 @app.route('/user/hospital', methods=['GET'])
 @login_required
-@cross_origin(origin='localhost', headers=['Content- Type', 'Authorization'])
+@cross_origin(headers=['Content- Type', 'Authorization'])
 def hospital():
     return Hospital().getAllHospitals()
 
 
 @app.route('/user/hospital/<id>', methods=['GET'])
 @login_required
-@cross_origin(origin='localhost', headers=['Content- Type', 'Authorization'])
+@cross_origin(headers=['Content- Type', 'Authorization'])
 def doctor_hs(id):
     return Hospital().getAllDoctors_by_Hospital(id)
 
 
 @app.route('/user/doctor/<id>', methods=['GET'])
 @login_required
-@cross_origin(origin='localhost', headers=['Content- Type', 'Authorization'])
+@cross_origin(headers=['Content- Type', 'Authorization'])
 def doctor(id):
     return Hospital().getDoctor(id)
--- routes/Lab.py
+++ routes/Lab.py
@@ -5,20 +5,20 @@ from utils.Decorator_Login import login_required
 
 @app.route('/user/lab', methods=['GET'])
 @login_required
-@cross_origin(origin='localhost', headers=['Content- Type', 'Authorization'])
+@cross_origin(headers=['Content- Type', 'Authorization'])
 def lab():
     return Lab().getAllLabs()
 
 
 @app.route('/user/lab/<id>', methods=['GET'])
 @login_required
-@cross_origin(origin='localhost', headers=['Content- Type', 'Authorization'])
+@cross_origin(headers=['Content- Type', 'Authorization'])
 def test_lab(id):
     return Lab().getAllTests_by_Lab(id)
 
 
 @app.route('/user/test/<id>', methods=['GET'])
 @login_required
-@cross_origin(origin='localhost', headers=['Content- Type', 'Authorization'])
+@cross_origin(headers=['Content- Type', 'Authorization'])
 def test(id):
     return Lab().getTest(id)
