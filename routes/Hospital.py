cd85db25e8443ac3be603b7a3e4d265be1be5daf
MichelK-Byte <michel.khoury.w@hotmail.com>
2021-05-02 10:27:05 +0300

    remove login required

--- routes/Hospital.py
+++ routes/Hospital.py
@@ -4,21 +4,18 @@ from utils.Decorator_Login import login_required
 
 
 @app.route('/user/hospital', methods=['GET'])
-@login_required
 @cross_origin(headers=['Content- Type', 'Authorization'])
 def hospital():
     return Hospital().getAllHospitals()
 
 
 @app.route('/user/hospital/<id>', methods=['GET'])
-@login_required
 @cross_origin(headers=['Content- Type', 'Authorization'])
 def doctor_hs(id):
     return Hospital().getAllDoctors_by_Hospital(id)
 
 
 @app.route('/user/doctor/<id>', methods=['GET'])
-@login_required
 @cross_origin(headers=['Content- Type', 'Authorization'])
 def doctor(id):
     return Hospital().getDoctor(id)
--- routes/Lab.py
+++ routes/Lab.py
@@ -4,21 +4,18 @@ from utils.Decorator_Login import login_required
 
 
 @app.route('/user/lab', methods=['GET'])
-@login_required
 @cross_origin(headers=['Content- Type', 'Authorization'])
 def lab():
     return Lab().getAllLabs()
 
 
 @app.route('/user/lab/<id>', methods=['GET'])
-@login_required
 @cross_origin(headers=['Content- Type', 'Authorization'])
 def test_lab(id):
     return Lab().getAllTests_by_Lab(id)
 
 
 @app.route('/user/test/<id>', methods=['GET'])
-@login_required
 @cross_origin(headers=['Content- Type', 'Authorization'])
 def test(id):
     return Lab().getTest(id)
